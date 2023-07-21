d04bf7d0ec499cbff73da1b9d3aa163ac1d9dab6
liu guoliang <liuguoliang@hanhou.cn>
2023-07-21 21:58:11 +0800

    生产环境去除打印信息

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import './common/rem'
 import {ElLoading,
   ElInfiniteScroll} from 'element-plus'
 import 'element-plus/dist/index.css'
+
+
+if (process.env.VUE_APP_SELF_ENV === 'prod') {
+  window.console.log = function (){};
+}
+
+
 const vueApp = createApp(App)
 vueApp.directive('loading', ElLoading)
 vueApp.directive('InfiniteScroll',ElInfiniteScroll)
