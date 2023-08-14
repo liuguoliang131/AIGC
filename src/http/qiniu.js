e876d25082eef4dd826725d112624f6ea6e6ab3a
liu guoliang <liuguoliang@hanhou.cn>
2023-08-14 19:53:15 +0800

    上传文件添加前缀userId

--- src/http/qiniu.js
+++ src/http/qiniu.js
@@ -1,58 +1,56 @@
 import * as qiniu from 'qiniu-js'
 import http from './index'
-import {
-	getToken
-} from '../common/utils.js'
+import utils from '../common/utils.js'
 
 
 let subscription = null
 
 // 获取七牛token方法
 const getQiniuToken = () => {
-	return new Promise(resolve => {
+  return new Promise(resolve => {
     http.get('/other/getUploadToken', {
     }).then(res => {
       if (res.code !== 200) {
         return resolve(false)
       }
-			resolve(res.data)
-		})
-	})
+      resolve(res.data)
+    })
+  })
 
 }
 
 // 命名file方法
 const setFileKey = (fileName) => {
-	try {
-		let type = fileName.split('.')
-		type = type[type.length - 1]
-		let token = 'null'
-		let time = new Date().getTime()
-		return token + '-' + time + '.' + type
-	} catch (e) {
-		//TODO handle the exception
-		return new Date().getTime() + '.' + 'png'
-	}
+  try {
+    let type = fileName.split('.')
+    type = type[type.length - 1]
+    const userId = utils.getUserInfo() ? utils.getUserInfo().userId : 'null'
+    let time = new Date().getTime()
+    return userId + '-' + time + '.' + type
+  } catch (e) {
+    //TODO handle the exception
+    return new Date().getTime() + '.' + 'png'
+  }
 
 }
 
 
 
 export const qiniuUpload = (file) => {
-	return new Promise(resolve => {
+  return new Promise(resolve => {
     getQiniuToken().then(data => {
       if (!data) {
         return resolve(false)
       }
-			const key = setFileKey(file.name)
+      const key = setFileKey(file.name)
       console.log('key', key)
-      const uphost = data.uploadUrl.includes('https://')?data.uploadUrl.replace('https://',''):data.uploadUrl
+      const uphost = data.uploadUrl.includes('https://') ? data.uploadUrl.replace('https://', '') : data.uploadUrl
 
       // 默认配置
       const config = {
         useCdnDomain: true, //是否使用cdn加速
         uphost: uphost, //上传域名
-        region:data.region, // 上传区域
+        region: data.region, // 上传区域
         upprotocol: 'https', //上传协议
         retryCount: 1, //上传自动重试次数（整体重试次数，而不是某个分片的重试次数）；默认 3 次（即上传失败后最多重试两次）
         debugLogLevel: 'info' //INFO | WARN | ERROR | OFF，允许程序在控制台输出日志，默认为 OFF，不输出任何日志，本功能仅仅用于本地调试，不建议在线上环境开启。
@@ -63,28 +61,28 @@ export const qiniuUpload = (file) => {
 
 
 
-			const observable = qiniu.upload(file, key, data.token, putExtra, config)
+      const observable = qiniu.upload(file, key, data.token, putExtra, config)
 
-			// 上传过程的监听函数
-			const observer = {
-				next(res) {
-					// ...
-					console.log('observer.success', res)
-				},
-				error(err) {
-					// ...
-					console.log('observer.error', err)
-				},
-				complete(res) {
-					// ...
-					console.log('observer.complete', res)
-					res.path = data.domain + '/' + res.key
-					resolve(res)
-				}
-			}
-			subscription = observable.subscribe(observer) // 上传开始
-			// subscription.unsubscribe() // 上传取消
-		})
-	})
+      // 上传过程的监听函数
+      const observer = {
+        next(res) {
+          // ...
+          console.log('observer.success', res)
+        },
+        error(err) {
+          // ...
+          console.log('observer.error', err)
+        },
+        complete(res) {
+          // ...
+          console.log('observer.complete', res)
+          res.path = data.domain + '/' + res.key
+          resolve(res)
+        }
+      }
+      subscription = observable.subscribe(observer) // 上传开始
+      // subscription.unsubscribe() // 上传取消
+    })
+  })
 
 }
