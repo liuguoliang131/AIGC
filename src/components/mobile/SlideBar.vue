55e889ec165a4f8a01c88ef05862dc259b6588cf
yk3372 <yk3372@gmail.com>
2023-09-11 20:15:06 +0800

    完成左侧菜单
    Signed-off-by: yk3372 <yk3372@gmail.com>

--- src/components/mobile/SlideBar.vue
+++ src/components/mobile/SlideBar.vue
@@ -1,44 +1,77 @@
 <template>
-  <div
-    :class="[
-      'slide_bar',
-      props.visible ? 'slide_bar-visible' : 'slide_bar-hidden',
-    ]"
-  >
-    <button @click="handHide">右上角X</button>
-    <button @click="handCreat">{{ from ? "新建对话" : "新建会话" }}</button>
-    <button @click="handGoAiHistory">
-      {{ from ? "历史对话" : "历史绘画" }}
-    </button>
-    <button @click="handGoAiIndex">
-      {{ from ? "快速进入AI绘画" : "快速进入AI对话" }}
-    </button>
-    <button @click="handGoLearnInfo">
-      {{
-        from
-          ? "AI对话课程上线了,赶快来看一看吧"
-          : "AI绘画课程上线了,赶快来看一看吧"
-      }}
-    </button>
-    <div>-----更多服务-------</div>
-    <button @click="handGoLearnIndex">学习中心</button>
-    <button @click="serviceVisible = true">联系客服</button>
-    <button @click="handExit">退出登录</button>
+  <div :class="[
+    'slide_bar',
+    props.visible ? 'slide_bar-visible' : 'slide_bar-hidden',
+  ]">
+    <div class="firstRow">
+      <span class="leftCount">剩余问答次数：{{ userStore.residueQAQuantity }}次</span>
+      <img @click="handHide" src="@/assets/icon_close_page.png" class="close" />
+    </div>
+    <div class="topEntry">
+      <div class="imgLeft">
+        <img @click="handCreat" :src="from ? 'https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_new_chat.png'
+          : 'https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_new_chat.png'
+          " class="imgLeftTop" />
+        <img @click="handGoAiHistory" :src="from ? 'https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_my_chat.png'
+          : 'https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_my_chat.png'
+          " class="imgLeftBottom" />
+      </div>
+      <img @click="handGoAiIndex" :src="from ? 'https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_enter_draw.png'
+        : 'https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_enter_draw.png'
+        " class="imgRightCenter" />
+    </div>
+    <img @click="handGoLearnInfo" :src="from ? 'https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_course.png'
+      : 'https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_course.png'
+      " class="imgCourse" />
+
+    <div class="footerWrapper">
+      <div class="footerTitle">更多服务</div>
+      <div @click="handGoLearnIndex" class="footerItemWrapper">
+        <img src="https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_learn_center.png"
+          style="width: 14px; height: 16.64px;" />
+        <span class="footer_item_title">学习中心</span>
+        <img src="https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_sidebar_more.png"
+          style="width: 12px; height: 12px;" />
+      </div>
+      <div class="divider"></div>
+      <div @click="serviceVisible = true" class="footerItemWrapper">
+        <img src="https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_contacts.png"
+          style="width: 14px; height: 16.64px;" />
+        <span class="footer_item_title">联系客服</span>
+        <img src="https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_sidebar_more.png"
+          style="width: 12px; height: 12px;" />
+      </div>
+
+      <div class="divider"></div>
+      <div @click="handExit" class="footerItemWrapper">
+        <img src="https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_exit.png" style="width: 14px; height: 16.64px;" />
+        <span class="footer_item_title">退出登录</span>
+        <img src="https://quanres.hanhoukeji.com/hanhou-ai-pc/mobile_sidebar_more.png"
+          style="width: 12px; height: 12px;" />
+      </div>
+    </div>
     <van-overlay :show="serviceVisible" @click="serviceVisible = false">
-      <div class="wrapper">
-        <div class="block" @click.stop>二维码</div>
+      <div class="service">
+        <div class="serviceWrapper">
+          <div class="service_title">扫描二维码添加专属客服</div>
+          <div class="service_code">
+            <img src="https://quanres.hanhoukeji.com/hanhou-ai-pc/CustomerServiceCode.png" alt="" />
+          </div>
+        </div>
       </div>
     </van-overlay>
   </div>
 </template>
 
 <script setup>
+import { ElRow, ElCol } from "element-plus";
 import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { showDialog, showToast, Overlay as VanOverlay } from "vant";
 import request from "@/http/index";
 import api from "@/http/api";
 import { useUserStore } from "@/store/user";
+import MyDialog from "@/components/MyDialog.vue";
 const userStore = useUserStore(); // 用户信息
 
 const props = defineProps({
@@ -154,7 +187,7 @@ const handExit = () => {
     .then(() => {
       confirmExit();
     })
-    .catch(() => {});
+    .catch(() => { });
 };
 </script>
 
@@ -167,25 +200,147 @@ const handExit = () => {
   width: 100%;
   height: 100%;
   box-sizing: border-box;
-  background: rgba(0, 0, 0, 0.5);
+  background: #222E50;
   transition: left 0.5s;
+  padding: 14px 16px;
+
+  .firstRow {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding-bottom: 14px;
+
+    .leftCount {
+      color: white;
+      font-size: 14px;
+    }
+
+    .close {
+      width: 22px;
+      height: 22px;
+    }
+  }
+
+  .topEntry {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 7px;
+
+    .imgLeft {
+      display: flex;
+      flex-direction: column;
+      margin-right: 6px;
+
+      .imgLeftTop {
+        width: 165px;
+        height: 54px;
+        margin-bottom: 6px;
+      }
+
+      .imgLeftBottom {
+        width: 165px;
+        height: 54px;
+      }
+    }
+
+    .imgRightCenter {
+      width: 172px;
+      height: 114px;
+    }
+  }
+
+  .imgCourse {
+    width: 343px;
+    height: 114px;
+    margin-bottom: 11px;
+  }
+
+  .footerWrapper {
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 4px;
+    padding: 16px 16px 0 16px;
+    display: flex;
+    flex-direction: column;
+
+    .divider {
+      height: 0.3px;
+      width: 100%;
+      background-color: rgba(255, 255, 255, 0.2);
+    }
+
+    .footerTitle {
+      color: white;
+      font-size: 16px;
+      font-weight: bold;
+      margin-bottom: 4px;
+    }
+
+    .footerItemWrapper {
+      display: flex;
+      padding: 12px 0;
+      flex-direction: row;
+      align-items: center;
+
+      .footer_item_title {
+        margin-left: 10px;
+        font-size: 14px;
+        flex: 1;
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+  }
 }
-.slide_bar-visible {
-  left: 0;
-}
-.slide_bar-hidden {
-  left: -100%;
-}
-.wrapper {
+
+.service {
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100%;
+
+  .serviceWrapper {
+    flex-direction: column;
+    background-color: white;
+    width: 280px;
+    border-radius: 12px;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+
+    .service_title {
+      margin-top: 30px;
+      color: #000;
+      font-family: PingFang SC;
+      font-size: 18px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+
+    .service_code {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-top: 30px;
+      margin-bottom: 30px;
+      width: 200px;
+      height: 200px;
+
+      img {
+        width: 200px;
+        height: 200px;
+      }
+    }
+  }
+
 }
 
-.block {
-  width: 120px;
-  height: 120px;
-  background-color: #fff;
+.slide_bar-visible {
+  left: 0;
+}
+
+.slide_bar-hidden {
+  left: -100%;
 }
 </style>
