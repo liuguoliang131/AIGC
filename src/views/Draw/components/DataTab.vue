5385cbc41382c340b8a603c0e5bebb1e02a964bc
yk3372 <yk3372@gmail.com>
2023-09-18 17:12:06 +0800

    修改hint文案
    Signed-off-by: yk3372 <yk3372@gmail.com>

--- src/views/Draw/components/DataTab.vue
+++ src/views/Draw/components/DataTab.vue
@@ -212,7 +212,7 @@ const madePicture4 = async () => {
         resize="none"
         maxlength="500"
         :autosize="false"
-        placeholder="请描述您想要的画面（目前支持中/英文，英文输入效果更佳）。"
+        placeholder="请描述您想要的画面"
       ></textarea>
       <div class="ipt_ass" v-if="baseData.pictureIdea.length">
         <div class="limit">
