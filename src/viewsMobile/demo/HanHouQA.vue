d72683d715429e86b2453e8b64fa7e8ce88d364b
yk3372 <yk3372@gmail.com>
2023-09-26 14:44:26 +0800

    问答完善语音
    Signed-off-by: yk3372 <yk3372@gmail.com>

--- src/viewsMobile/demo/HanHouQA.vue
+++ src/viewsMobile/demo/HanHouQA.vue
@@ -6,17 +6,46 @@
       </van-dropdown-menu>
 
       <van-cell-group inset>
-        <van-field v-model="message" rows="5" autosize type="textarea" maxlength="500" placeholder="请输入您的问题"
-          show-word-limit />
+        <van-field
+          v-model="message"
+          rows="5"
+          autosize
+          type="textarea"
+          maxlength="500"
+          placeholder="请输入您的问题"
+          show-word-limit
+        />
       </van-cell-group>
       <div class="btnWrapper">
         <div class="reset" @click="reset">重置</div>
-        <div class="submit" @click="answer">回答</div>
+        <voice-input-button
+        appId="57fe6cc5"
+        apiKey="0bf1bd1b6e154aea50622ef5bf4257c6"
+        apiSecret="MTI4MjVjNmQxMjBkYTZiZjI3N2U5MjBi"
+        v-model="result"
+        @record="showResult"
+        @record-start="recordStart"
+        @record-stop="recordStop"
+        @record-blank="recordNoResult"
+        @record-failed="recordFailed"
+        @record-ready="recordReady"
+        @record-complete="recordComplete"
+        interactiveMode="press"
+        tipPosition="top"
+      >
+        <template slot="no-speak">没听清您说的什么</template>
+      </voice-input-button>
+      <div class="submit" @click="answer">手动<br>提问</div>
       </div>
     </div>
 
     <div class="result" @click="copy">{{ result }}</div>
-    <div ref="copy_text" class="clipboard_text" data-clipboard-action="copy" data-clipboard-text="copytext"></div>
+    <div
+      ref="copy_text"
+      class="clipboard_text"
+      data-clipboard-action="copy"
+      data-clipboard-text="copytext"
+    ></div>
   </div>
 </template>
 
@@ -24,6 +53,8 @@
 import { ref } from "vue";
 
 import request from "@/http/index";
+import voiceInputButton from "./input/voice-input-button.vue";
+
 import Clipboard from "clipboard";
 import {
   CellGroup as VanCellGroup,
@@ -35,7 +66,7 @@ import {
   showToast,
 } from "vant";
 const selectResult = ref("/qa/hanhou");
-const message = ref('');
+const message = ref("");
 const showPicker = ref(false);
 const columns = [
   { text: "憨猴", value: "/qa/hanhou" },
@@ -57,28 +88,55 @@ function answer() {
     forbidClick: true,
   });
   const query = encodeURIComponent(message.value);
-  request.get(`/user/hanhouqa?question=${selectResult.value}/${query}`).then((res) => {
-    closeToast();
-    if (res.code == 200) {
-      result.value = res.data;
-    } else {
-      showToast(res.msg || res.message);
-    }
-  }).catch(() => {
-    closeToast();
-  });
+  request
+    .get(`/user/hanhouqa?question=${selectResult.value}/${query}`)
+    .then((res) => {
+      closeToast();
+      if (res.code == 200) {
+        result.value = res.data;
+      } else {
+        showToast(res.msg || res.message);
+      }
+    })
+    .catch(() => {
+      closeToast();
+    });
+}
+function recordReady() {
+  console.info("按钮就绪!");
+}
+function recordStart() {
+  console.info("录音开始");
+}
+function showResult(text) {
+  message.value = text;
+  console.info("收到识别结果：", text);
+}
+function recordStop() {
+  console.info("录音结束");
+}
+function recordNoResult() {
+  console.info("没有录到什么，请重试");
+}
+function recordComplete(text) {
+  message.value = text;
+  answer();
+  console.info("识别完成! 最终结果：", text);
+}
+function recordFailed(error) {
+  console.info("识别失败，错误栈：", error);
 }
 
 function reset() {
-  message.value = ''
-  result.value = ''
+  message.value = "";
+  result.value = "";
 }
 
 // 复制
 const copy_text = ref();
 const copy = () => {
   const text1 = result.value;
-  if (text1.trim() === '') return;
+  if (text1.trim() === "") return;
   copy_text.value.setAttribute("data-clipboard-text", text1);
   initCopyClipboard();
   copy_text.value.click();
@@ -88,12 +146,12 @@ const copy = () => {
 const initCopyClipboard = () => {
   const clipboard = new Clipboard(".clipboard_text");
   clipboard.on("success", function (e) {
-    showToast('复制成功');
+    showToast("复制成功");
     // 清空选中
     clipboard.destroy();
   });
   clipboard.on("error", function (e) {
-    showToast('复制失败');
+    showToast("复制失败");
     // 清空选中
     clipboard.destroy();
   });
@@ -146,12 +204,13 @@ const initCopyClipboard = () => {
     display: flex;
     flex-direction: row;
     margin-top: 20px;
+    align-items: center;
     .submit {
       display: flex;
       align-items: center;
       justify-content: center;
-      width: 120px;
-      height: 40px;
+      width: 60px;
+      height: 60px;
       flex-shrink: 0;
       margin-left: 10px;
       border-radius: 5px;
@@ -173,8 +232,8 @@ const initCopyClipboard = () => {
       display: flex;
       align-items: center;
       justify-content: center;
-      width: 120px;
-      height: 40px;
+      width: 60px;
+      height: 60px;
       flex-shrink: 0;
       border-radius: 5px;
       background: #dfa634;
